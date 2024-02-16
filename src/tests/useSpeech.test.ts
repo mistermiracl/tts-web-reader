a5012eb2bfaf7aa4bb781f21e9767634d8565505
mistermiracl <avaldezn07@gmail.com>
2024-02-16 03:07:31 -0500

    test: mock window.speechSynthesis

--- src/tests/useSpeech.test.ts
+++ src/tests/useSpeech.test.ts
@@ -13,6 +13,18 @@ import { useSpeech } from '../lib/useSpeech';
 
 describe("useSpeech Test Suite", () => {
   it("should return current sentence idx and current word range as well as playback state", () => {
+    window.speechSynthesis = {
+      cancel() {
+      },
+      pause() {
+      },
+      speak() {
+      },
+      getVoices() {
+        return [];
+      }
+    } as any;
+    window.SpeechSynthesisUtterance = function () {} as any;
     const sentences = ["This is a sentence.", "This is another sentence."];
     const { result } = renderHook(() => useSpeech(sentences));
     expect(result.current.currentSentenceIdx).toBe(0);
